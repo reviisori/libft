e28f9e4770858fe2de2ac7fbf02cf6006fae972a
Aleksandr Tikka <altikka@student.hive.fi>
2022-06-15 18:15:11 +0300

    creates a fresh vector with given initial size

---
+++ ft_vecnew.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_vecnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/25 09:33:48 by altikka           #+#    #+#             */
+/*   Updated: 2022/04/27 14:15:25 by altikka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vec.h"
+
+int	ft_vecnew(t_vec *dest, size_t init_len, size_t type)
+{
+	if (!dest || init_len == 0 || type == 0)
+		return (-1);
+	dest->alloc_size = type * init_len;
+	dest->type = type;
+	dest->len = 0;
+	if (init_len == 0)
+		dest->data = NULL;
+	else
+	{
+		dest->data = malloc(dest->alloc_size);
+		if (!dest->data)
+		{
+			dest->alloc_size = 0;
+			dest->type = 0;
+			return (-1);
+		}
+	}
+	return (1);
+}
