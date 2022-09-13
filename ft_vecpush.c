1eea5c6b070fc4d2b826d295fd44c6460e99b4bd
Aleksandr Tikka <altikka@student.hive.fi>
2022-09-13 16:33:38 +0300

    uses RESIZE_FAC now instead of dead set integer

--- ft_vecpush.c
+++ ft_vecpush.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 17:26:52 by altikka           #+#    #+#             */
-/*   Updated: 2022/06/15 19:49:46 by altikka          ###   ########.fr       */
+/*   Updated: 2022/09/13 14:09:54 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_vecpush(t_vec *dest, const void *src)
 		if (ft_vecnew(dest, 1, dest->type) < 0)
 			return (-1);
 	if (dest->type * dest->len >= dest->alloc_size)
-		if (ft_vecresize(dest, dest->alloc_size * 2) < 0)
+		if (ft_vecresize(dest, dest->alloc_size * RESIZE_FAC) < 0)
 			return (-1);
 	ft_memcpy(&dest->data[dest->type * dest->len], src, dest->type);
 	dest->len++;
