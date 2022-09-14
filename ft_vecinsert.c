06cdbb0b2b3b3d21990eb5f6123310f04927b54d
Aleksandr Tikka <altikka@student.hive.fi>
2022-09-14 15:43:59 +0300

    can insert src at any given place (index) of the vector's data
    if the given place is at the end of the data, pushes it there. else makes room for it and places it in between existing data

---
+++ ft_vecinsert.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_vecinsert.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/14 11:21:35 by altikka           #+#    #+#             */
+/*   Updated: 2022/09/14 15:40:34 by altikka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "vec.h"
+
+int	ft_vecinsert(t_vec *dest, const void *src, size_t index)
+{
+	if (!dest || !src || index > dest->len)
+		return (-1);
+	if (index == dest->len)
+		return (ft_vecpush(dest, src));
+	if (dest->type * dest->len >= dest->alloc_size)
+		if (ft_vecresize(dest, dest->alloc_size * RESIZE_FAC) < 0)
+			return (-1);
+	ft_memmove(
+		ft_vecget(dest, index + 1),
+		ft_vecget(dest, index),
+		(dest->len - index) * dest->type);
+	ft_memcopy(
+		ft_vecget(dest, index),
+		src,
+		dest->type);
+	dest->len++;
+	return (1);
+}
