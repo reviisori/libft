375b6c2fb877c02ba51036b7586c8746beb17d3e
Aleksandr Tikka <altikka@student.hive.fi>
2022-11-11 16:09:30 +0200

    now returns t_hash instead of just t_hash.value

--- hash_get.c
+++ hash_get.c
@@ -6,13 +6,13 @@
 /*   By: altikka & atenhune <@student.hive.fi>       +#+  +:+       +#+       */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 13:18:50 by altikka           #+#    #+#             */
-/*   Updated: 2022/11/01 17:34:02 by altikka          ###   ########.fr       */
+/*   Updated: 2022/11/11 16:07:47 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	hash_get(t_hashmap *src, char *key)
+t_hash	*hash_get(t_hashmap *src, char *key)
 {
 	size_t	org;
 	size_t	try;
@@ -24,9 +24,9 @@ int	hash_get(t_hashmap *src, char *key)
 	while (i < src->capacity && src->entries[try].key != NULL)
 	{
 		if (!ft_strncmp(key, src->entries[try].key, ft_strlen(key) + 1))
-			return (src->entries[try].value);
+			return (&src->entries[try]);
 		i++;
 		try = (i + org) % src->capacity;
 	}
-	return (0);
+	return (NULL);
 }
