eab5047bc6243313f5d27b39bf76f3fc35e3cd87
Aleksandr Tikka <altikka@student.hive.fi>
2022-11-28 15:55:13 +0200

    function that converts ascii to long

---
+++ ft_atol.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/28 15:47:41 by altikka           #+#    #+#             */
+/*   Updated: 2022/11/28 15:51:25 by altikka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long	ft_atol(char const *s)
+{
+	long	result;
+	int		sign;
+	int		i;
+
+	result = 0;
+	sign = 1;
+	i = 0;
+	while (ft_isspace(s[i]) == 1)
+		i++;
+	if (s[i] == '-')
+	{
+		sign *= -1;
+		i++;
+	}
+	else if (s[i] == '+')
+		i++;
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
